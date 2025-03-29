b322cf358277d1102e5307b5ec55fbe720de5feb
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2025-03-29 15:19:37 -0300

    Criar servidor.java

---
+++ src/com/example/servidor.java
@@ -0,0 +1,11 @@
+package com.exemplo.servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuServidorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeuServidorApplication.class, args);
+    }
+}
