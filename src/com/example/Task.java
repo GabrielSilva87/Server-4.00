19c6204aa63333b46688013997544c86b59b741e
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2025-03-29 15:20:06 -0300

    Criar Task.java

---
+++ src/com/example/Task.java
@@ -0,0 +1,32 @@
+// model/Task.java
+package com.exemplo.meuservidor.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
