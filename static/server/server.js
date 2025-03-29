fe5e78f79b92fb8060a0b2508127156a4793e927
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2025-03-29 15:41:25 -0300

    Atualizar o server.js

--- static/server/server.js
+++ static/server/server.js
@@ -4,16 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Rota de exemplo
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Init server');
 });
 
-// Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
